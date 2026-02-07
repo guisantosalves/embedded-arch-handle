b211e0ae9ff2115c11a02881fee2d2c310833c3b
guisantosalves <workguisantos@gmail.com>
2026-02-07 14:48:29 -0400

    fixing the algorithm

--- main.go
+++ main.go
@@ -1,16 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/go-vgo/robotgo"
 )
 
 func main() {
+
+	fmt.Println("Iniciando anti-idle para Teams...")
+	fmt.Println("Pressione Ctrl+C para parar o programa.")
+
+	// intervalor para mover o mouse a cada 30 segundos
+	intervalo := 5 * time.Second
+
+	ticker := time.NewTicker(intervalo)
+
+	// Quando a função terminar, executa Stop() para liberar recursos e evitar leak de memória
+	defer ticker.Stop()
+
 	for {
-		x, y := robotgo.GetMousePos()
-		robotgo.MoveSmooth(x+10, y)
-		robotgo.MoveSmooth(x-10, y)
-		time.Sleep(30 * time.Second)
+		select {
+		case <-ticker.C:
+			// x, y := robotgo.GetMousePos()
+			// robotgo.MoveSmooth(x+10, y)
+			// robotgo.MoveSmooth(x-10, y)
+			// time.Sleep(30 * time.Second)
+			executeAction()
+		}
 	}
+}
+
+func executeAction() {
+	robotgo.KeyTap("scroll_lock")
+	time.Sleep(100 * time.Second)
+	robotgo.KeyTap("scroll_lock")
 }
